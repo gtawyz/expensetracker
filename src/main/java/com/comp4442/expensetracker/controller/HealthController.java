7bb570339c8abbfd0d3eaabd08f28803ca551473
gtawyz <gtawyz@gmail.com>
2026-04-08 23:55:11 +0800

    Initial commit

---
+++ src/main/java/com/comp4442/expensetracker/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.comp4442.expensetracker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/api/health")
+    public String health() {
+        return "Smart Expense Tracker API is running.";
+    }
+}
---
+++ src/test/java/com/comp4442/expensetracker/ExpensetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comp4442.expensetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensetrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
